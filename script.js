0afd243ee03bf697d5e9ea1ebfc7b26a1386fccb
KwabenaNkrumahRichard <nkrumahrichard62@gmail.com>
2024-05-02 19:14:49 +0000

    Added html, css and js files

---
+++ script.js
@@ -0,0 +1,43 @@
+
+let container = document.querySelector('.container');
+let button = document.querySelector('button');
+
+
+function createGrid(size){
+    for(i =0; i<(size*size);i++){
+        let div = document.createElement('div');
+        div.classList.add('item');
+        container.append(div);
+
+        // COLOR GENERATOR
+        let r = Math.floor(Math.random()*265);
+        let g = Math.floor(Math.random()*265);
+        let b = Math.floor(Math.random()*265);
+        let rgb = `rgb(${r},${g},${b})`;
+        
+        // MOUSE HOVER EVENT
+        div.addEventListener('mouseover', function(){
+            div.style.backgroundColor = rgb ;
+        })
+    }
+
+    container.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    container.style.gridTemplateRows = `repeat(${size},1fr)`;
+}
+
+
+
+button.addEventListener('click', function(){
+    container.innerText ="";
+    let gridSize = prompt('Enter the number of squares for your new grid(1-100 inclusive)');
+    gridSize = Number(gridSize);
+
+    if(gridSize > 0 && gridSize <=100){
+        createGrid(gridSize);
+    }
+    else
+        alert('Enter number between 1-100(INCLUSIVE)');
+});
+
+
+createGrid(16);
